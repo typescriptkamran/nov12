b3cbaabe51ce24d5138109a7dc01c687894821b8
typescriptkamran <typescriptwithkamran@gmail.com>
2023-12-12 20:36:28 +0500

    update

--- src/components/react/Board.tsx
+++ src/components/react/Board.tsx
@@ -2,12 +2,14 @@
 import React, { useState } from "react";
 import Box from "./Boxex";
 
+type BoxValues = Array<null | string>;
+
 const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
-  const [boxes, setBoxes] = useState<Array<null | string>>(Array(9).fill(null));
+  const [boxes, setBoxes] = useState<BoxValues>(Array(9).fill(null));
 
   function handleClick(i: number) {
-    if (boxes[i] !== null) {
+    if (boxes[i] || calculateWinner(boxes))  {
       return;
     }
     const nextBoxes = boxes.slice();
@@ -20,8 +22,37 @@ const Board = () => {
     setXIsNext(!xIsNext);
   }
 
+  function calculateWinner(boxes: BoxValues) {
+    const lines: Array<Array<number>> = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
+        return boxes[a];
+      }
+    }
+    return null;
+  }
+  const winner = calculateWinner(boxes);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+
   return (
     <div>
+      <div className="status">{status}</div>
       <div>
         <Box value={boxes[0]} onBoxClick={() => handleClick(0)} />
         <Box value={boxes[1]} onBoxClick={() => handleClick(1)} />
