9d973cf64522d7782e970ab2392a0d78f6da2374
typescriptkamran <typescriptwithkamran@gmail.com>
2023-12-18 02:06:07 +0500

    update

--- src/components/to-do-app/Card.tsx
+++ src/components/to-do-app/Card.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { MdMoreTime } from "react-icons/md";
+import React from 'react';
+import { MdMoreTime } from 'react-icons/md';
 
-
-const card = () => {
+const Card = () => {
   return (
     <div>
-        <div className="relative w-60 h-72 rounded-[60px] bg-zinc-900/90 p-5 m-5">
-            <div className="absolute left-0 top-0 rounded-t-[60px] text-xl font-bold text-zinc-300 bg-slate-400 h-20 w-full flex gap-2 py-5 px-5 justify-center">
-                <MdMoreTime />
-                <h1 className='text-center'>title of card</h1>
-            </div>
-            <p className='py-2 px-2 mt-16'>
-                Lorem ipsum dolor sit amet consectetur adipisicing.
-            </p>
-            <div className="absolute bottom-0 left-0 bg-zinc-600 w-full h-20 rounded-b-[60px] text-center ">
-                this is footer
-            </div>
-
+      <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 p-5 m-5">
+        <div className="absolute left-0 top-0 rounded-t-[50px] text-xl font-bold text-zinc-300 bg-slate-400 h-15 w-full flex gap-2 py-5 px-5 justify-center">
+          <MdMoreTime />
+          <h1 className="text-center">title of card</h1>
         </div>
+        <p className="py-2 px-2 mt-16">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+        <div className="absolute bottom-0 left-0 bg-zinc-600 w-full h-20 rounded-b-[50px] flex items-center justify-center">
+          <div className="flex flex-col gap-4">
+            <button className="btn btn--primary">task completed</button>
+            <button className="btn btn--secondary">edit task</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default card
+export default Card;
