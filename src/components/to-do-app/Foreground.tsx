0f638ce0a79521af7ea8faf4cf6038f190967990
--g <--g>
2023-12-17 17:21:10 +0500

    update

--- src/components/to-do-app/Foreground.tsx
+++ src/components/to-do-app/Foreground.tsx
@@ -5,12 +5,17 @@ const Foreground = () => {
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-zinc-800/50 z-[3]'>
       <div className='relitive w-60 h-60 rounded-lg bg-zinc-900/90 p-5'>
-      <RiTodoFill />
-      <p className='text-white text-sm leading-tight '>
-        Lorem ipsum dolor, sit amet consectetur adipisicing.
-      </p>
+        <RiTodoFill />
+        <p className='text-white text-sm leading-tight '>
+          Lorem ipsum dolor, sit amet consectetur adipisicing.
+        </p>
+        
+        <div className="absolute bottom-0 bg-white w-f h-20 left-0"></div>
 
-        <div className='absolute bottom-5 right-5 p-2'></div>
+
+
+
+        
       </div>
 
 
