dc2ab6386dc364c8e34d1959ab7efdfd6a0e184a
typescriptkamran <typescriptwithkamran@gmail.com>
2023-12-24 22:21:52 +0500

    update

--- src/app/(react)/react/customhook/page.tsx
+++ src/app/(react)/react/customhook/page.tsx
@@ -3,10 +3,11 @@ import { useLocalStorage } from '@/components/utilis/useLocalStorage'
 import React from 'react'
 import { useState } from 'react'
 
+
 const page = () => {
     let [Value, setValue] = useState('')
  
-    const [setItem] = useLocalStorage('value')
+    const [setItem, getItem] = useLocalStorage('value')
 
 
 
@@ -20,7 +21,7 @@ const page = () => {
             className="bg-slate-300 p-3 w-96 rounded-md" /> 
             <div className='flex flex-row gap-4 m-5'>
                 <button className='btn btn--primary  ' onClick={() => setItem(Value)}>Set</button>
-                <button className='btn  btn--primary'>Get</button>
+                <button className='btn  btn--primary' onClick={() => console.log(getItem())} >Get</button>
                 <button className='btn  btn--primary'>Remove</button>
                 
 
--- src/components/utilis/useLocalStorage.ts
+++ src/components/utilis/useLocalStorage.ts
@@ -1,9 +1,28 @@
 export const useLocalStorage = (key: string) => {
     const setItem = (value: unknown) => {
-        window.localStorage.setItem(key, JSON.stringify(value))
+        try {
+            window.localStorage.setItem(key, JSON.stringify(value))
+
+        } catch (error) {
+            console.log(error)
+
+        }                   
         
     }
 
-    return [setItem]
+    
+        const getItem = (value: unknown) => {
+            try {
+                const item = window.localStorage.getItem(key)
+                return item ? JSON.parse(item) : undefined
+    
+            } catch (error) {
+                console.log(error)
+    
+            }                   
+            
+        }
+
+    return [setItem, getItem]
     
 }  
